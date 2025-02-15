8b97b3033e4b4950c56fc7500a3c6bbae3ee6f68
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2025-02-15 18:17:27 +0100

    changed login page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,8 +9,8 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36"></div>
+        <div className="flex items-center justify-center h-20 w-full rounded-lg bg-blue-500 p-3 md:h-36">
+          <h1 className="flex text-5xl  text-white">Flashcards</h1>
         </div>
         <LoginForm />
       </div>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -23,14 +23,13 @@ export default function LoginForm() {
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-4 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
                 required
               />
-              {/* <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
             </div>
           </div>
           <div className="mt-4">
@@ -42,7 +41,8 @@ export default function LoginForm() {
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-4
+                 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
                 name="password"
@@ -50,7 +50,6 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              {/* <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
             </div>
           </div>
         </div>
@@ -60,7 +59,6 @@ export default function LoginForm() {
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
             <>
-              {/* <ExclamationCircleIcon className="h-5 w-5 text-red-500" /> */}
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
